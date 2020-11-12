bfc6e687dce5ca6247d98e733efe385ebf9cf975
Jim Anderson <jima.coding@gmail.com>
2020-11-11 18:13:32 -0700

    Fixing messed up merge that I did on Brad's PR

--- rplint/__init__.py
+++ rplint/__init__.py
@@ -1,6 +1,7 @@
 from .main import TestLineLen
 from .main import TestBadWords
 from .main import TestPhrases
+from .main import TestCodeBlockOrAlertEndsSection
 from .main import TestContractions
 from .main import TestCodeFormatter
 from .main import TestLeadingColon
