4261968dec98beaaff4cb06e99fed3e66499369c
Leodanis Pozo Ramos <lpozor78@gmail.com>
2023-07-23 14:50:23 +0200

    Update imports with new namings.

--- rplint/__init__.py
+++ rplint/__init__.py
@@ -1,11 +1,10 @@
-from .main import TestLineLen
-from .main import TestBadWords
-from .main import TestHereLinks
-from .main import TestPhrases
-from .main import TestCodeBlockOrAlertEndsSection
-from .main import TestContractions
-from .main import TestCodeFormatter
-from .main import TestLeadingColon
-from .main import rplint
-
-__version__ = "0.7.1"
+from .main import BadLinkAnchorCheck  # noqa
+from .main import BadPhrasesCheck  # noqa
+from .main import BadWordsCheck  # noqa
+from .main import CodeBlockOrAlertEndsSectionCheck  # noqa
+from .main import CodeFormatterCheck  # noqa
+from .main import ContractionsCheck  # noqa
+from .main import EndingColonCheck  # noqa
+from .main import LineLengthCheck  # noqa
+from .main import __version__  # noqa
+from .main import rplint  # noqa
